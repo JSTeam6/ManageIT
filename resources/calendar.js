9087ceec76439c6d63d50c3fee1f044fe2feaf7e
Georgi Spasov <georgi.spasov@outlook.com>
2017-10-06 18:32:41 +0300

    Refactored

--- resources/calendar.js
+++ resources/calendar.js
@@ -1,32 +1,28 @@
-$(document).ready(function () {
-    const calendarDiv = $('#calendar')
+const getCalendar = function () {
     const currentDate = new Date()
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let currentMonth = currentDate.getMonth()
+    let monthName = monthNames[currentMonth]
+    let currentYear = currentDate.getFullYear()
 
-    calendarDiv.append('<div id="year">' + currentDate.getFullYear())
-    const yearDiv = $('#year')
+    $('#calendar').append(`<div id="year">${currentYear}</div>`)
+    $('#year').append(`<div id="month">${monthName}<br></div>`)
 
-    const currentMonth = currentDate.getMonth()
-
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August",
-        "September", "October", "November", "December"]
-    let monthToString = months[currentMonth]
-
-    yearDiv.append('<div id="month">' + monthToString + '<br>')
-    let month = currentDate.getMonth()
-    let year = currentDate.getFullYear()
     let numDays = 31
-    if ((month == 3) || (month == 5) || (month == 8) || (month == 10)) {
+    if ((currentMonth == 3) || (currentMonth == 5) || (currentMonth == 8) || (currentMonth == 10)) {
         numDays = 30
-    }
-    if ((month == 1)) {
+    } else if ((currentMonth == 1)) {
         numDays = 28
-        if ((year % 4 == 0) && (year % 100 != 0)) {
+        if ((currentYear % 4 == 0) && (currentYear % 100 != 0)) {
             numDays = 29
         }
-        if (year % 400 == 0) {
+        if (currentYear % 400 == 0) {
             numDays = 29
         }
     }
+
+    let dayOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay()
+
     const monthID = $('#month')
     monthID.append('<div id="monday">' + "Monday")
     const monday = $('#monday')
@@ -42,114 +38,24 @@ $(document).ready(function () {
     const saturday = $('#saturday')
     monthID.append('<div id="sunday">' + "Sunday")
     const sunday = $('#sunday')
+    const outerWeekdayDivs = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
 
-    let firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)
-    let dayOfWeek = firstDayOfMonth.getDay()
-
-    const outerDivSunday = document.getElementById('sunday')
-    const outerDivMonday = document.getElementById('monday')
-    const outerDivTuesday = document.getElementById('tuesday')
-    const outerDivWednesday = document.getElementById('wednesday')
-    const outerDivThursday = document.getElementById('thursday')
-    const outerDivFriday = document.getElementById('friday')
-    const outerDivSaturday = document.getElementById('saturday')
-    switch (dayOfWeek) {
-        case 0:
-            monday.append('<div class="empty">' + '<br>')
-            tuesday.append('<div class="empty">' + '<br>')
-            wednesday.append('<div class="empty">' + '<br>')
-            thursday.append('<div class="empty">' + '<br>')
-            friday.append('<div class="empty">' + '<br>')
-            saturday.append('<div class="empty">' + '<br>')
-            break
-        case 2:
-            monday.append('<div class="empty">' + '<br>')
-            break
-        case 3:
-            monday.append('<div class="empty">' + '<br>')
-            tuesday.append('<div class="empty">' + '<br>')
-            break
-        case 4:
-            monday.append('<div class="empty">' + '<br>')
-            tuesday.append('<div class="empty">' + '<br>')
-            wednesday.append('<div class="empty">' + '<br>')
-            break
-        case 5:
-            monday.append('<div class="empty">' + '<br>')
-            tuesday.append('<div class="empty">' + '<br>')
-            wednesday.append('<div class="empty">' + '<br>')
-            thursday.append('<div class="empty">' + '<br>')
-            break
-        case 6:
-            monday.append('<div class="empty">' + '<br>')
-            tuesday.append('<div class="empty">' + '<br>')
-            wednesday.append('<div class="empty">' + '<br>')
-            thursday.append('<div class="empty">' + '<br>')
-            friday.append('<div class="empty">' + '<br>')
-            break
-    }
-
+    let tempStartDay = dayOfWeek + 1
     for (let i = 1; i <= numDays; i++) {
-
-        switch (dayOfWeek) {
-            case 0:
-                let divSunday = document.createElement('div')
-                divSunday.className = "BOX"
-                divSunday.id = i + '_' + month + '_' + year
-                divSunday.textContent = i
-                outerDivSunday.appendChild(divSunday)
-                dayOfWeek++
-                break
-            case 1:
-                let divMonday = document.createElement('div')
-                divMonday.className = "BOX"
-                divMonday.id = i + '_' + month + '_' + year
-                divMonday.textContent = i
-                outerDivMonday.appendChild(divMonday)
-                dayOfWeek++
-                break
-            case 2:
-                let divTuesday = document.createElement('div')
-                divTuesday.className = "BOX"
-                divTuesday.id = i + '_' + month + '_' + year
-                divTuesday.textContent = i
-                outerDivTuesday.appendChild(divTuesday)
-                dayOfWeek++
-                break
-            case 3:
-                let divWednesday = document.createElement('div')
-                divWednesday.className = "BOX"
-                divWednesday.id = i + '_' + month + '_' + year
-                divWednesday.textContent = i
-                outerDivWednesday.appendChild(divWednesday)
-                dayOfWeek++
-                break
-            case 4:
-                let divThursday = document.createElement('div')
-                divThursday.className = "BOX"
-                divThursday.id = i + '_' + month + '_' + year
-                divThursday.textContent = i
-                outerDivThursday.appendChild(divThursday)
-                dayOfWeek++
-                break
-            case 5:
-                let divFriday = document.createElement('div')
-                divFriday.className = "BOX"
-                divFriday.id = i + '_' + month + '_' + year
-                divFriday.textContent = i
-                outerDivFriday.appendChild(divFriday)
-                dayOfWeek++
-                break
-            case 6:
-                let divSaturday = document.createElement('div')
-                divSaturday.className = "BOX"
-                divSaturday.id = i + '_' + month + '_' + year
-                divSaturday.textContent = i
-                outerDivSaturday.appendChild(divSaturday)
-                dayOfWeek = 0
-                break
+        if (tempStartDay < 7) {
+            outerWeekdayDivs[tempStartDay].append('<div class="empty">' + '<br>')
+            tempStartDay++
+            i--
+        } else {
+            let newDayDiv = document.createElement('div')
+            newDayDiv.className = "BOX"
+            newDayDiv.id = i + '_' + currentMonth + '_' + currentYear
+            newDayDiv.textContent = i
+            outerWeekdayDivs[dayOfWeek % 7].append(newDayDiv)
+            dayOfWeek++
         }
     }
-    //break
+}
 
-})
+$(document).ready(getCalendar()
+)
